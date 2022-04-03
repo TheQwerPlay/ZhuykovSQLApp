f8fb46d2470888a153cc937d5c693ef2dd2339cd
TheQwerPlay <bogdanzhuickov35@gmail.com>
2022-04-03 23:00:31 +0400

    First commit

---
+++ ZhuykovSQLApp/LoginForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ZhuykovSQLApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.passField = new System.Windows.Forms.TextBox();
+            this.loginField = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("GOST type A", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(300, 350);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ещё нет аккаунта?";
+            this.label3.MouseEnter += new System.EventHandler(this.label3_MouseEnter);
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            // 
+            // passField
+            // 
+            this.passField.BackColor = System.Drawing.Color.Black;
+            this.passField.Font = new System.Drawing.Font("GOST type A", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passField.ForeColor = System.Drawing.Color.Gray;
+            this.passField.Location = new System.Drawing.Point(230, 250);
+            this.passField.Name = "passField";
+            this.passField.Size = new System.Drawing.Size(320, 42);
+            this.passField.TabIndex = 9;
+            this.passField.MouseEnter += new System.EventHandler(this.passField_MouseEnter);
+            this.passField.MouseLeave += new System.EventHandler(this.passField_MouseLeave);
+            // 
+            // loginField
+            // 
+            this.loginField.BackColor = System.Drawing.Color.Black;
+            this.loginField.Font = new System.Drawing.Font("GOST type A", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginField.ForeColor = System.Drawing.Color.Gray;
+            this.loginField.Location = new System.Drawing.Point(230, 200);
+            this.loginField.Name = "loginField";
+            this.loginField.Size = new System.Drawing.Size(320, 42);
+            this.loginField.TabIndex = 8;
+            this.loginField.MouseEnter += new System.EventHandler(this.loginField_MouseEnter);
+            this.loginField.MouseLeave += new System.EventHandler(this.loginField_MouseLeave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Font = new System.Drawing.Font("GOST type A", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(250, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(286, 45);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::ZhuykovSQLApp.Properties.Resources.glasOpen;
+            this.pictureBox4.Location = new System.Drawing.Point(550, 250);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ZhuykovSQLApp.Properties.Resources.glasClose;
+            this.pictureBox3.Location = new System.Drawing.Point(550, 250);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ZhuykovSQLApp.Properties.Resources._lock;
+            this.pictureBox2.Location = new System.Drawing.Point(180, 250);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ZhuykovSQLApp.Properties.Resources.user1;
+            this.pictureBox1.Location = new System.Drawing.Point(180, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.passField);
+            this.Controls.Add(this.loginField);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passField;
+        private System.Windows.Forms.TextBox loginField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
---
+++ ZhuykovSQLApp/LoginForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZhuykovSQLApp
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginField_MouseEnter(object sender, EventArgs e)
+        {
+            if (loginField.Text == "someone@example.com")
+            {
+                loginField.Text = "";
+                loginField.ForeColor = Color.White;
+            }
+        }
+
+        private void loginField_MouseLeave(object sender, EventArgs e)
+        {
+            if (loginField.Text == "")
+            {
+                loginField.Text = "someone@example.com";
+                loginField.ForeColor = Color.Gray;
+            }
+        }
+
+        private void passField_MouseEnter(object sender, EventArgs e)
+        {
+            if (passField.Text == "password")
+            {
+                passField.Text = "";
+                passField.UseSystemPasswordChar = true;
+                passField.ForeColor = Color.White;
+            }
+        }
+
+        private void passField_MouseLeave(object sender, EventArgs e)
+        {
+            if (passField.Text == "")
+            {
+                passField.Text = "password";
+                passField.UseSystemPasswordChar = false;
+
+                passField.ForeColor = Color.Gray;
+            }
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            loginField.Text = "someone@example.com";
+            passField.Text  = "password";
+        }
+
+        private void label3_MouseEnter(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.White;
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.Gray;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if (passField.UseSystemPasswordChar == true)
+            {
+                pictureBox3.Visible = false;
+                passField.UseSystemPasswordChar = false;
+                pictureBox4.Visible = true;
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (passField.UseSystemPasswordChar == false)
+            {
+                pictureBox4.Visible = false;
+                passField.UseSystemPasswordChar = true;
+                pictureBox3.Visible = true;
+            }
+        }
+    }
+}
