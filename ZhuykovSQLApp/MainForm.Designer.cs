2d2ed202a0e55a86b68939e7a2f48bcd1e9ed02b
TheQwerPlay <102906341+TheQwerPlay@users.noreply.github.com>
2022-04-07 03:08:25 +0400

    Справка + вывод БД

--- ZhuykovSQLApp/MainForm.Designer.cs
+++ ZhuykovSQLApp/MainForm.Designer.cs
@@ -28,7 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.candyTableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sweetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supplierToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.arrivalLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.referenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(784, 537);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.candyTableToolStripMenuItem,
+            this.referenceToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // candyTableToolStripMenuItem
+            // 
+            this.candyTableToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sweetsToolStripMenuItem,
+            this.supplierToolStripMenuItem1,
+            this.arrivalLogToolStripMenuItem});
+            this.candyTableToolStripMenuItem.Name = "candyTableToolStripMenuItem";
+            this.candyTableToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.candyTableToolStripMenuItem.Text = "Выбор таблицы";
+            // 
+            // sweetsToolStripMenuItem
+            // 
+            this.sweetsToolStripMenuItem.Name = "sweetsToolStripMenuItem";
+            this.sweetsToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.sweetsToolStripMenuItem.Text = "Конфеты";
+            this.sweetsToolStripMenuItem.Click += new System.EventHandler(this.sweetsToolStripMenuItem_Click);
+            // 
+            // supplierToolStripMenuItem1
+            // 
+            this.supplierToolStripMenuItem1.Name = "supplierToolStripMenuItem1";
+            this.supplierToolStripMenuItem1.Size = new System.Drawing.Size(192, 22);
+            this.supplierToolStripMenuItem1.Text = "Поставщики";
+            this.supplierToolStripMenuItem1.Click += new System.EventHandler(this.supplierToolStripMenuItem1_Click);
+            // 
+            // arrivalLogToolStripMenuItem
+            // 
+            this.arrivalLogToolStripMenuItem.Name = "arrivalLogToolStripMenuItem";
+            this.arrivalLogToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.arrivalLogToolStripMenuItem.Text = "Журнал поступления";
+            this.arrivalLogToolStripMenuItem.Click += new System.EventHandler(this.arrivalLogToolStripMenuItem_Click);
+            // 
+            // referenceToolStripMenuItem
+            // 
+            this.referenceToolStripMenuItem.Name = "referenceToolStripMenuItem";
+            this.referenceToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.referenceToolStripMenuItem.Text = "Справка";
+            this.referenceToolStripMenuItem.Click += new System.EventHandler(this.referenceToolStripMenuItem_Click);
             // 
             // MainForm
             // 
@@ -36,16 +105,32 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(800, 600);
             this.MinimumSize = new System.Drawing.Size(800, 600);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MainForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem candyTableToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sweetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supplierToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem arrivalLogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem referenceToolStripMenuItem;
+        private System.Windows.Forms.HelpProvider helpProvider1;
     }
 }
--- ZhuykovSQLApp/MainForm.cs
+++ ZhuykovSQLApp/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,74 @@ namespace ZhuykovSQLApp
 {
     public partial class MainForm : Form
     {
+        DateBase db;
+        DataTable table = null;
+        MySqlDataAdapter adapter = null;
         public MainForm()
         {
             InitializeComponent();
+            db = new DateBase();
+            db.openConnection();
+        }
+
+        private void sweetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Clear();
+            adapter = new MySqlDataAdapter("SELECT * FROM `sweets`", db.getConnection());
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+
+        }
+
+        private void supplierToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Clear();
+            adapter = new MySqlDataAdapter("SELECT * FROM `supplier`", db.getConnection());
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        private void arrivalLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Clear();
+            adapter = new MySqlDataAdapter("SELECT * FROM `arrivallog`", db.getConnection());
+            //ALTER TABLE `arrivallog` ADD FOREIGN KEY(`id_supplier`) REFERENCES `supplier`(`id_supplier`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            db.closeConnection();
+        }        
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            table = new DataTable();
+            if (dataGridView1.Columns[dataGridView1.CurrentCell.ColumnIndex].HeaderText.ToString() == "id_sweet")
+            {
+                table.Clear();
+                string s = dataGridView1.CurrentCell.Value.ToString();
+                adapter = new MySqlDataAdapter($"SELECT * FROM `sweets` WHERE `id_sweet` = {s}", db.getConnection());
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+            if (dataGridView1.Columns[dataGridView1.CurrentCell.ColumnIndex].HeaderText.ToString() == "id_supplier")
+            {
+                table.Clear();
+                string s = dataGridView1.CurrentCell.Value.ToString();
+                adapter = new MySqlDataAdapter($"SELECT * FROM `supplier` WHERE `id_supplier` = {s}", db.getConnection());
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+            }
+        }
+
+        private void referenceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this, "ZhuykovSQLAppHelp.chm");
         }
     }
 }
